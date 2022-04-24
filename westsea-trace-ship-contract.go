ae26230a11540cf25bd95f12be32a3ba76a85ee4
pedroei <pedroaraujo_41@hotmail.com>
2022-04-24 20:39:59 +0100

    changes based on the new data model

--- westsea-trace-ship-contract.go
+++ westsea-trace-ship-contract.go
@@ -19,14 +19,40 @@ type WestseaTraceShipContract struct {
 /*
  ****************************************
  ****************************************
- * ProductSerialNum TRANSCATION METHDOS *
+ * ProductLot TRANSCATION METHDOS *
  ****************************************
  ****************************************
  */
 
-// ProductSerialNumExists returns true when asset with given ID exists in world state
-func (c *WestseaTraceShipContract) ProductSerialNumExists(ctx contractapi.TransactionContextInterface, productSerialNumID string) (bool, error) {
-	data, err := ctx.GetStub().GetState(productSerialNumID)
+ // GetAllProductLot retrieves all instances of ProductLot from the world state
+func (c *WestseaTraceShipContract) GetAllProductLot(ctx contractapi.TransactionContextInterface) ([]*ProductLot, error) {
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+	  return nil, err
+	}
+	defer resultsIterator.Close()
+  
+	var productLots []*ProductLot
+	for resultsIterator.HasNext() {
+	  queryResponse, err := resultsIterator.Next()
+	  if err != nil {
+		return nil, err
+	  }
+  
+	  var productLot ProductLot
+	  err = json.Unmarshal(queryResponse.Value, &productLot)
+	  if err != nil {
+		return nil, err
+	  }
+	  productLots = append(productLots, &productLot)
+	}
+  
+	return productLots, nil
+}
+
+// ProductLotExists returns true when asset with given ID exists in world state
+func (c *WestseaTraceShipContract) ProductLotExists(ctx contractapi.TransactionContextInterface, productLotID string) (bool, error) {
+	data, err := ctx.GetStub().GetState(productLotID)
 
 	if err != nil {
 		return false, err
@@ -35,117 +61,175 @@ func (c *WestseaTraceShipContract) ProductSerialNumExists(ctx contractapi.Transa
 	return data != nil, nil
 }
 
-// CreateProductSerialNum creates a new instance of WestseaTraceShip
-func (c *WestseaTraceShipContract) CreateProductSerialNum(ctx contractapi.TransactionContextInterface, 
-	productSerialNumID string, 
-	serialNumber string,
+// CreateProductLot creates a new instance of WestseaTraceShip
+func (c *WestseaTraceShipContract) CreateProductLot(ctx contractapi.TransactionContextInterface, 
+	productLotID string, 
+	referenceNumber string,
+	isSerialNumber bool,
 	designation string,
 	productType string,
+	initialAmount float32,
 	documentKeys []string,
 	) (string, error) {
 
-	exists, err := c.ProductSerialNumExists(ctx, productSerialNumID)
+	exists, err := c.ProductLotExists(ctx, productLotID)
 	if err != nil {
 		return "", fmt.Errorf("could not read from world state. %s", err)
 	} else if exists {
-		return "", fmt.Errorf("the lot %s already exists", productSerialNumID)
+		return "", fmt.Errorf("the lot %s already exists", productLotID)
 	}
 
-	productSerialNum := &ProductSerialNum{
-		DocType: "productSerialNum",
-		ID:      productSerialNumID,
-		SerialNumber: serialNumber,
-		Designation: designation,
-		ProductType: productType,
-		DocumentKeys: documentKeys,
+	//the referenceNumber of a productLot can be the serial number, if isSerialNumber; or the lotNumber if !isSerialNumber)
+	if isSerialNumber {
+		initialAmount = 1
 	}
 
-	bytes, err := json.Marshal(productSerialNum)
+	productLot := &ProductLot{
+		DocType:           "productLot",
+		ID:                productLotID,
+		ReferenceNumber:   referenceNumber,
+		IsSerialNumber:    isSerialNumber,
+		Designation:       designation,
+		ProductType:       productType,
+		InitialQuantity:   initialAmount,
+		AvailableQuantity: initialAmount,
+		DocumentKeys:      documentKeys,
+	}
+
+	bytes, err := json.Marshal(productLot)
 	if err != nil {
 		return "", err
 	}
 
-	err = ctx.GetStub().PutState(productSerialNum.ID, bytes)
+	err = ctx.GetStub().PutState(productLot.ID, bytes)
 	if err != nil {
 		return "", fmt.Errorf("failed to put to world state: %v", err)
 	}
 
-	return fmt.Sprintf("%s created successfully", productSerialNumID), nil
+	return fmt.Sprintf("%s created successfully", productLotID), nil
 }
 
-// ReadProductSerialNum retrieves an instance of ProductSerialNum from the world state
-func (c *WestseaTraceShipContract) ReadProductSerialNum(ctx contractapi.TransactionContextInterface, productSerialNumID string) (*ProductSerialNum, error) {
-	exists, err := c.ProductSerialNumExists(ctx, productSerialNumID)
+// ReadProductLot retrieves an instance of ProductLot from the world state
+func (c *WestseaTraceShipContract) ReadProductLot(ctx contractapi.TransactionContextInterface, productLotID string) (*ProductLot, error) {
+	exists, err := c.ProductLotExists(ctx, productLotID)
 	if err != nil {
 		return nil, fmt.Errorf("Could not read from world state. %s", err)
 	} else if !exists {
-		return nil, fmt.Errorf("The asset %s does not exist", productSerialNumID)
+		return nil, fmt.Errorf("The asset %s does not exist", productLotID)
 	}
 
-	bytes, _ := ctx.GetStub().GetState(productSerialNumID)
+	bytes, _ := ctx.GetStub().GetState(productLotID)
 
-	productSerialNum := new(ProductSerialNum)
+	productLot := new(ProductLot)
 
-	err = json.Unmarshal(bytes, productSerialNum)
+	err = json.Unmarshal(bytes, productLot)
 
 	if err != nil {
 		return nil, fmt.Errorf("Could not unmarshal world state data to type WestseaTraceShip")
 	}
 
-	return productSerialNum, nil
+	return productLot, nil
 }
 
-// UpdateProductSerialNumDocumentKeys retrieves an instance of ProductSerialNum from the world state and updates its document keys
-func (c *WestseaTraceShipContract) UpdateProductSerialNumDocumentKeys(ctx contractapi.TransactionContextInterface, productSerialNumID string, newDocumentKeys []string) (string, error) {
-	exists, err := c.ProductSerialNumExists(ctx, productSerialNumID)
+// UpdateProductLotDocumentKeys retrieves an instance of ProductLot from the world state and updates its document keys
+func (c *WestseaTraceShipContract) UpdateProductLotDocumentKeys(ctx contractapi.TransactionContextInterface, productLotID string, newDocumentKeys []string) (string, error) {
+	exists, err := c.ProductLotExists(ctx, productLotID)
 	if err != nil {
 		return "", fmt.Errorf("Could not read from world state. %s", err)
 	} else if !exists {
-		return "", fmt.Errorf("The asset %s does not exist", productSerialNumID)
+		return "", fmt.Errorf("The asset %s does not exist", productLotID)
 	}
 
-	outdatedProductSerialNumBytes, _ := ctx.GetStub().GetState(productSerialNumID) // Gets "old" ProductSerialNum bytes from productSerialNumID
+	outdatedProductLotBytes, _ := ctx.GetStub().GetState(productLotID) // Gets "old" ProductLot bytes from productLotID
 
-	outdatedProductSerialNum := new(ProductSerialNum) // Initialize outdated/"old" ProductSerialNum object
+	outdatedProductLot := new(ProductLot) // Initialize outdated/"old" ProductLot object
 
-	// Parses the JSON-encoded data in bytes (outdatedProductSerialNumBytes) to the "old" ProductSerialNum object (outdatedProductSerialNum)
-	err = json.Unmarshal(outdatedProductSerialNumBytes, outdatedProductSerialNum)
+	// Parses the JSON-encoded data in bytes (outdatedProductLotBytes) to the "old" ProductLot object (outdatedProductLot)
+	err = json.Unmarshal(outdatedProductLotBytes, outdatedProductLot)
 	if err != nil {
-		return "", fmt.Errorf("could not unmarshal world state data to type ProductSerialNum")
+		return "", fmt.Errorf("could not unmarshal world state data to type ProductLot")
 	}
 
-	productSerialNum := &ProductSerialNum{
-		DocType: "productSerialNum",
-		ID:      productSerialNumID,
-		SerialNumber: outdatedProductSerialNum.SerialNumber,
-		Designation: outdatedProductSerialNum.Designation,
-		ProductType: outdatedProductSerialNum.ProductType,
+	productLot := &ProductLot{
+		DocType: "productLot",
+		ID:      productLotID,
+		ReferenceNumber: outdatedProductLot.ReferenceNumber,
+		IsSerialNumber: outdatedProductLot.IsSerialNumber,
+		Designation: outdatedProductLot.Designation,
+		ProductType: outdatedProductLot.ProductType,
+		InitialQuantity:   outdatedProductLot.InitialQuantity,
+		AvailableQuantity: outdatedProductLot.AvailableQuantity,
 		DocumentKeys: newDocumentKeys,
 	}
 
-	bytes, _ := json.Marshal(productSerialNum)
+	bytes, _ := json.Marshal(productLot)
 	if err != nil {
 		return "", err
 	}
 
-	err = ctx.GetStub().PutState(productSerialNumID, bytes)
+	err = ctx.GetStub().PutState(productLotID, bytes)
 	if err != nil {
 		return "", fmt.Errorf("failed to put to world state: %v", err)
 	}
 
-	return fmt.Sprintf("%s document keys updated successfully", productSerialNumID), nil
+	return fmt.Sprintf("%s document keys updated successfully", productLotID), nil
 }
 
-// DeleteProductSerialNum deletes an instance of ProductSerialNum from the world state
-func (c *WestseaTraceShipContract) DeleteProductSerialNum(ctx contractapi.TransactionContextInterface, productSerialNumID string) error {
-	exists, err := c.ProductSerialNumExists(ctx, productSerialNumID)
+
+
+// UpdateProductAvailableAmount retrieves an instance of ProductLot from the world state and updates its available quantity
+func (c *WestseaTraceShipContract) UpdateProductAvailableQuantity(ctx contractapi.TransactionContextInterface, productLotID string, newAvailableQuantity float32) (string, error) {
+	exists, err := c.ProductLotExists(ctx, productLotID)
+	if err != nil {
+		return "", fmt.Errorf("Could not read from world state. %s", err)
+	} else if !exists {
+		return "", fmt.Errorf("The asset %s does not exist", productLotID)
+	}
+
+	outdatedProductLotBytes, _ := ctx.GetStub().GetState(productLotID) // Gets "old" ProductLot bytes from productLotID
+
+	outdatedProductLot := new(ProductLot) // Initialize outdated/"old" ProductLot object
+
+	// Parses the JSON-encoded data in bytes (outdatedProductLotBytes) to the "old" ProductLot object (outdatedProductLot)
+	err = json.Unmarshal(outdatedProductLotBytes, outdatedProductLot)
+	if err != nil {
+		return "", fmt.Errorf("could not unmarshal world state data to type ProductLot")
+	}
+
+	productLot := &ProductLot{
+		DocType: "productLot",
+		ID:      productLotID,
+		ReferenceNumber: outdatedProductLot.ReferenceNumber,
+		IsSerialNumber: outdatedProductLot.IsSerialNumber,
+		Designation: outdatedProductLot.Designation,
+		ProductType: outdatedProductLot.ProductType,
+		InitialQuantity:   outdatedProductLot.InitialQuantity,
+		AvailableQuantity: newAvailableQuantity,
+		DocumentKeys: outdatedProductLot.DocumentKeys,
+	}
+
+	bytes, _ := json.Marshal(productLot)
+	if err != nil {
+		return "", err
+	}
+
+	err = ctx.GetStub().PutState(productLotID, bytes)
+	if err != nil {
+		return "", fmt.Errorf("failed to put to world state: %v", err)
+	}
+
+	return fmt.Sprintf("%s available quantity updated successfully to %.2f", productLotID, newAvailableQuantity), nil
+}
+// DeleteProductLot deletes an instance of ProductLot from the world state
+func (c *WestseaTraceShipContract) DeleteProductLot(ctx contractapi.TransactionContextInterface, productLotID string) error {
+	exists, err := c.ProductLotExists(ctx, productLotID)
 	if err != nil {
 		return fmt.Errorf("Could not read from world state. %s", err)
 	} else if !exists {
-		return fmt.Errorf("The asset %s does not exist", productSerialNumID)
+		return fmt.Errorf("The asset %s does not exist", productLotID)
 	}
 
-	return ctx.GetStub().DelState(productSerialNumID)
+	return ctx.GetStub().DelState(productLotID)
 }
 
 
@@ -175,8 +259,8 @@ func (c *WestseaTraceShipContract) CreateActivity(ctx contractapi.TransactionCon
 	designation string,
 	userId string,
 	dateTime string,
-	inputProductSerialNums []string,
-	outputProductSerialNum ProductSerialNum,
+	inputProductLots map[string]float32,
+	outputProductLot ProductLot,
 	) (string, error) {
 
 	exists, err := c.ActivityExists(ctx, activityID)
@@ -186,31 +270,52 @@ func (c *WestseaTraceShipContract) CreateActivity(ctx contractapi.TransactionCon
 		return "", fmt.Errorf("the asset %s already exists", activityID)
 	}
 
-	//the input products must exist
-	for _, inputID := range inputProductSerialNums {
-
-	   exists, err = c.ProductSerialNumExists(ctx, inputID)
+	for inputID, usedAmount  := range inputProductLots {
+		
+		//the input products must exist
+	   exists, err = c.ProductLotExists(ctx, inputID)
 	   if err != nil {
 		   return "", fmt.Errorf("Could not read from world state. %s", err)
 	   } else if !exists {
 		   return "", fmt.Errorf("The input product [%s] does not exists", inputID)
 	   }
+
+	   //get product
+	   product, err := c.ReadProductLot(ctx, inputID)
+		if err != nil {
+			return "", fmt.Errorf("Could not read from world state. %s", err)
+		}
+
+		//make sure the inputProduct quantity is valid quantities
+		
+		if usedAmount <= 0 {
+			return "", fmt.Errorf("inputProduct amount must be greater than 0 (inputProduct amount for inputProduct [%s] is %.2f)", inputID, usedAmount)
+		} else if usedAmount > product.AvailableQuantity {
+			return "", fmt.Errorf("inputProduct amount must not exceed the inputProduct availableQuantity (inputProduct [%s] maximum quantity is %.2f)", inputID, product.AvailableQuantity)
+		}
+
+		//the amounts on inputLots should be reduced on the availableAmount of a productLot
+		_, err = c.UpdateProductAvailableQuantity(ctx, inputID, product.AvailableQuantity - usedAmount)
+		if err != nil {
+			return "", fmt.Errorf("Could not write to world state. %s", err)
+		}
    }
 
-	//FIXME: O produto output deve ser criado previamente? Ou durante a criação de uma atividade?
-	exists, err = c.ActivityExists(ctx, outputProductSerialNum.ID)
+	exists, err = c.ProductLotExists(ctx, outputProductLot.ID)
 	if err != nil {
 		return "", fmt.Errorf("Could not read from world state. %s", err)
 	} else if exists {
-		return "", fmt.Errorf("The output product [%s] already exists", outputProductSerialNum.ID)
+		return "", fmt.Errorf("The output product [%s] already exists", outputProductLot.ID)
 	}
 
-	_, err = c.CreateProductSerialNum(ctx, 
-		outputProductSerialNum.ID,
-		outputProductSerialNum.SerialNumber,
-		outputProductSerialNum.Designation,
-		outputProductSerialNum.ProductType,
-		outputProductSerialNum.DocumentKeys,
+	_, err = c.CreateProductLot(ctx, 
+		outputProductLot.ID,
+		outputProductLot.ReferenceNumber,
+		outputProductLot.IsSerialNumber,
+		outputProductLot.Designation,
+		outputProductLot.ProductType,
+		outputProductLot.InitialQuantity,
+		outputProductLot.DocumentKeys,
 	)
 	if err != nil {
 		return "", fmt.Errorf("could not read from world state. %s", err)
@@ -222,8 +327,8 @@ func (c *WestseaTraceShipContract) CreateActivity(ctx contractapi.TransactionCon
 		Designation: designation,
 		UserId: userId,
 		DateTime: dateTime,
-		InputProductSerialNums: inputProductSerialNums,
-		OutputProductSerialNum: outputProductSerialNum,
+		InputProductLots: inputProductLots,
+		OutputProductLot: outputProductLot,
 	}
 
 	bytes, err := json.Marshal(activity)
@@ -261,8 +366,6 @@ func (c *WestseaTraceShipContract) ReadActivity(ctx contractapi.TransactionConte
 	return activity, nil
 }
 
-//TODO: should we be able to update any property of the activity?
-
 // DeleteActivity deletes an instance of Activity from the world state
 func (c *WestseaTraceShipContract) DeleteActivity(ctx contractapi.TransactionContextInterface, activityID string) error {
 	exists, err := c.ActivityExists(ctx, activityID)
@@ -274,3 +377,5 @@ func (c *WestseaTraceShipContract) DeleteActivity(ctx contractapi.TransactionCon
 
 	return ctx.GetStub().DelState(activityID)
 }
+
+//TODO: remove deletes
--- westsea-trace-ship.go
+++ westsea-trace-ship.go
@@ -4,24 +4,28 @@
 
 package main
 
-//FIXME: a product can only be on ONE activity?
-type ProductSerialNum struct {
-	DocType 	 	string   `json:"docType"`
-	ID      	 	string   `json:"ID"`
-	SerialNumber 	string   `json:"serialNumber"`
-	Designation  	string   `json:"designation"`
-	ProductType  	string   `json:"productType"`
-	DocumentKeys 	[]string `json:"documentKeys,omitempty" metadata:"documentKeys,optional"`
-	// BuilderActivity string 	 `json:"builderActivity"` //activity that originated the product
+type ProductLot struct {
+	DocType 	 	  string   `json:"docType"`
+	ID      	 	  string   `json:"ID"`
+	ReferenceNumber	  string   `json:"referenceNumber"`
+	IsSerialNumber    bool 	   `json:"isSerialNumber"`
+	Designation  	  string   `json:"designation"`
+	ProductType  	  string   `json:"productType"`
+	InitialQuantity   float32  `json:"initialQuantity"`
+	AvailableQuantity float32  `json:"availableQuantity"`
+	DocumentKeys 	  []string `json:"documentKeys,omitempty" metadata:"documentKeys,optional"`
 }
 
-//FIXME: the outputs can be more than one ??
 type Activity struct {
-	DocType 	 			string   			`json:"docType"`
-	ID      	 			string   			`json:"ID"`
-	Designation  			string   			`json:"designation"`
-	UserId  				string   			`json:"userId"` //user that performed the activity
-	DateTime 				string 	 			`json:"dateTime"`
-	InputProductSerialNums 	[]string 			`json:"inputProductSerialNums"`
-	OutputProductSerialNum 	ProductSerialNum   	`json:"outputProductSerialNum"` //FIXME: should this be just the ID?
+	DocType 	 	 string   			`json:"docType"`
+	ID      	 	 string   			`json:"ID"`
+	Designation		 string   			`json:"designation"`
+	UserId  		 string   			`json:"userId"` //user that performed the activity
+	DateTime    	 string 	 		`json:"dateTime"`
+	InputProductLots map[string]float32 `json:"inputProductLots"`
+	OutputProductLot ProductLot   	    `json:"outputProductLot"` //FIXME: should this be just the ID?
 }
+
+//TODO: endpoint to get all products
+//TODO: endpoint to get all activities
+//TODO: endpoint to get traceability: send a referenceNumber and return all activities that product is a part of and all the products that made that product
